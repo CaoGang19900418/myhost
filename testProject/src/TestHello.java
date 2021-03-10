51d0da80afba78c7f765e088ca71d15c94f40333
caogang <972869684@qq.com>
2021-03-10 21:52:49 +0800

    three

--- testProject/src/TestHello.java
+++ testProject/src/TestHello.java
@@ -1,5 +1,23 @@
+import java.util.UUID;
+
+/**
+ * 测试
+ */
 public class TestHello {
+
     public static void main(String args[]){
         System.out.println("Hello World!! my");
+        String randomId = getUUID();
+        System.out.println(randomId);
     }
+
+    /**
+     * 获取随机32uuid
+     * @return
+     */
+    public static String getUUID(){
+        String seqId = UUID.randomUUID().toString().replaceAll("-","");
+        return seqId;
+    }
+
 }
